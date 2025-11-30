f4912b71c94061a9ef55b23e5e8d1ae08011a7c3
OHMMYCodeEz <48040670+OHMMYCodeEz@users.noreply.github.com>
2025-12-01 05:59:21 +0700

    Update Catch-a-Monster.lua

--- Catch-a-Monster.lua
+++ Catch-a-Monster.lua
@@ -192,7 +192,10 @@ local function collectAllEggsInArea()
                         VirtualInputManager:SendKeyEvent(true, "E", false, game)
                         wait(1.5)
                         VirtualInputManager:SendKeyEvent(false, "E", false, game)
-
+                        wait(0.5)
+                        VirtualInputManager:SendKeyEvent(true, "E", false, game)
+                        wait(1.5)
+                        VirtualInputManager:SendKeyEvent(false, "E", false, game)
                         collectedCount = collectedCount + 1
                         print("Collected egg: " .. eggCrate.Name .. " (" .. collectedCount .. ")")
                     else
